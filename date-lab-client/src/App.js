cc55c76227ba2cfdaa4c57a8cd4a702ffc433d85
David Morrin <dm187@nyu.edu>
2020-07-24 16:23:53 -0400

    feat: loops response back into input
    Adds button to use the database response date as the new date input.
    
    If all is setup correctly, the date should remain fixed.
    
    If the date shifts, there is an issue somewhere.

--- date-lab-client/src/App.js
+++ date-lab-client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Button, Container } from "@material-ui/core";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
-// import MomentUtils from "@date-io/moment";
 import moment from "moment";
 import DateFnsUtils from "@date-io/date-fns";
 import { lightFormat } from "date-fns/fp";
@@ -15,15 +14,28 @@ const print = (anything) => JSON.stringify(anything, null, 2);
 const dateInspect = (date) => ({
   "date instanceof Date?": date instanceof Date,
   toString: date.toString(),
-  toJSON: date.toJSON(),
+  toJSON: date.toJSON ? date.toJSON() : "n/a",
   "date-fns": {
-    formatJSON: formatJSON(date),
+    formatJSON: date instanceof Date ? formatJSON(date) : "n/a",
   },
   moment: {
     "format-no-arg": moment(date).format(),
     "format-JSON": moment(date).format(ISOfmt),
   },
 });
+
+const responseInspect = ({ error, data }) => {
+  if (error || !data || !data.length) return { error };
+  const { date } = data[0];
+  const dateObj = new Date(date);
+  return {
+    data,
+    date: {
+      typeof: typeof date,
+      asDate: dateObj,
+    },
+  };
+};
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -46,23 +58,36 @@ function App() {
       })
       .catch(setError);
 
+  const setResponseToSelectedDate = () => {
+    if (!results) return setError(new Error("no results yet"));
+    const { data } = results;
+    if (!data || !data.length) return setError(new Error("no data..."));
+    const { date } = data[0];
+    setSelectedDate(new Date(date.split(".")[0]));
+  };
+
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Container>
         <DateTimePicker value={selectedDate} onChange={setSelectedDate} />
         <Button onClick={submit}>Send to MySQL</Button>
+        {results && (
+          <Button onClick={setResponseToSelectedDate}>
+            Set response as input
+          </Button>
+        )}
         <h2>current date value inspection</h2>
         <pre>{print(dateInspect(selectedDate))}</pre>
         {error && (
           <>
             <h2>error</h2>
-            <pre>{print(error)}</pre>
+            <pre>{print(error.toString())}</pre>
           </>
         )}
         {results && (
           <>
             <h2>server response</h2>
-            <pre>{print(results)}</pre>
+            <pre>{print(responseInspect(results))}</pre>
           </>
         )}
       </Container>
